c290cae75f5b358e53535cbf95a92beb86726428
baganalytics <engr.jamesmartin@gmail.com>
2018-11-12 15:33:50 -0800

    Added pagination function

--- example.py
+++ example.py
@@ -9,28 +9,28 @@ citeauth = tokens.citeauth
 #Example Connection
 citeconn = sc.citelineConnection(citeuser,citepass,citeauth)
 
-
-#Example of citeline get_permissions
-#response = sc.queryApi.citelineGetPermissions(citeconn)
-
-#Example of citeline schema
-#change out the string "drugcatlyst" for one of the schemas present in get_permissions
-#response = sc.queryApi.citelineSchema("drugcatalyst", citeconn)
-
-#Example of a feed
-#response = sc.queryApi.citelineFeed("trial", citeconn)
-
-#Example of pagination
-response = sc.queryApi.citelineFeed("trial", citeconn)
-next_page = response['pagination']['nextPage']
-response = sc.queryApi.citelineFeed("trial", citeconn, has_page=next_page)
-
-#Example of a search
-# search_term = {
-#     "sponsorname":"Bayer",
-#     "trialstartdatefrom": "2017-01-01"
-# }
-# response = sc.queryApi.citelineSearch("trial", search_term, citeconn)
+#
+# Example of citeline get_permissions
+# response = sc.queryApi.citelineGetPermissions(citeconn)
+#
+# Example of citeline schema
+# change out the string "drugcatlyst" for one of the schemas present in get_permissions
+# response = sc.queryApi.citelineSchema("trial", citeconn)
+#
+# Example of a feed
+# response = sc.queryApi.citelineFeed("trial", citeconn)
+#
+# Example of pagination
+# response = sc.queryApi.citelineFeed("trial", citeconn)
+# next_page = response['pagination']['nextPage']
+# response = sc.queryApi.citelineFeed("trial", citeconn, has_page=next_page)
+#
+# Example of a search
+search_term = {
+    "sponsorname":"Bayer",
+    "trialstartdatefrom": "2017-01-01"
+}
+response = sc.queryApi.citelineSearch("trial", search_term, citeconn)
 
 pp = pprint.PrettyPrinter(indent=0,depth=3)
 pp.pprint(response)
